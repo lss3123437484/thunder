a18a92f6feb7860d4b06f62352e06fbb53c086fe
lss <3123437484@qq.com>
2022-07-02 15:45:47 +0800

    Initial commit

---
+++ src/test/java/com/thunder/ThunderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thunder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThunderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
